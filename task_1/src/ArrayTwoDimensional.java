bc7142959a808287388d179a5cd836c56164fbc4
Maksim Kravchenko <sr76@mail.ru>
2022-08-28 10:58:41 +0700

    2D array

--- task_1/src/ArrayTwoDimensional.java
+++ task_1/src/ArrayTwoDimensional.java
@@ -1,19 +1,25 @@
+import java.math.BigInteger;
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 //Заполните двумерный массив случайным числами и выведите максимальное, минимальное и среднее значение.
 
-public class Main {
+public class ArrayTwoDimensional {
 
     public static void main(String[] args) {
-
         int[][] array = new int[5][5];
 
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
+        BigInteger sum = BigInteger.ZERO;
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 int value = ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+                assert (value < 0x80000000);
+                assert (value > 0x7fffffff);
+
                 array[i][j] = value;
 
                 if (min > value) {
@@ -23,21 +29,32 @@ public class Main {
                 if (max < value) {
                     max = value;
                 }
+
             }
         }
 
-        int middle = (max + max) / 2;
+        assert (array == null);
 
-        for (int i = 0; i < array.length; i++) {
+        assert (!Objects.equals(sum, BigInteger.ZERO));
+
+        sum = sum.add(BigInteger.valueOf(max));
+        sum = sum.add(BigInteger.valueOf(min));
+        BigInteger average = sum.divide(BigInteger.valueOf(2));
+
+        System.out.println("Заполненный двумерный массив: \n");
+
+        for (int[] ints : array) {
             for (int j = 0; j < array.length; j++) {
-                System.out.print(array[i][j] + " ");
+                System.out.print(ints[j] + " ");
             }
             System.out.println();
         }
 
-        System.out.printf("Минимальное значение - %d \n",  min);
-        System.out.printf("Максимальное значение - %d \n", max);
-        System.out.printf("Среднее значение - %d", max);
+        System.out.println();
+
+        System.out.printf("Минимальное значение:  %d \n", min);
+        System.out.printf("Максимальное значение: %d \n", max);
+        System.out.printf("Среднее значение:      %d", average);
     }
 
 }
