d9f42ddb5771dcc132396aa911a600c3a2ce07a4
Maksim Kravchenko <sr76@mail.ru>
2022-08-29 15:02:51 +0700

    new design

--- task_1/src/ArrayTwoDimensional.java
+++ task_1/src/ArrayTwoDimensional.java
@@ -1,60 +1,81 @@
 import java.math.BigInteger;
-import java.util.Objects;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.*;
 
 //Заполните двумерный массив случайным числами и выведите максимальное, минимальное и среднее значение.
 
 public class ArrayTwoDimensional {
 
     public static void main(String[] args) {
-        int[][] array = new int[5][5];
+        int[][] array = init(10);
+        int[] result = calc(array);
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        System.out.printf("Максимальное значение: %d \n", result[2]);
+        System.out.printf("Минимальное значение:  %d \n", result[0]);
+        System.out.printf("Среднее значение:      %d", result[1]);
+    }
+
+    private static int[] calc(int[][] array) {
+        int[] result = {Integer.MAX_VALUE, 0, Integer.MIN_VALUE};
+
         BigInteger sum = BigInteger.ZERO;
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                int value = ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
-
-                assert (value < 0x80000000);
-                assert (value > 0x7fffffff);
-
-                array[i][j] = value;
-
-                if (min > value) {
-                    min = value;
-                }
-
-                if (max < value) {
-                    max = value;
-                }
-
-            }
-        }
-
-        assert (array == null);
-
-        assert (!Objects.equals(sum, BigInteger.ZERO));
-
-        sum = sum.add(BigInteger.valueOf(max));
-        sum = sum.add(BigInteger.valueOf(min));
-        BigInteger average = sum.divide(BigInteger.valueOf(2));
-
-        System.out.println("Заполненный двумерный массив: \n");
 
         for (int[] ints : array) {
             for (int j = 0; j < array.length; j++) {
-                System.out.print(ints[j] + " ");
+                int value = ints[j];
+
+                if (result[0] > value) {
+                    result[0] = value;
+                }
+
+                if (result[2] < value) {
+                    result[2] = value;
+                }
+
+                sum = sum.add(BigInteger.valueOf(value));
+
             }
-            System.out.println();
         }
 
-        System.out.println();
+        assert (!Objects.equals(sum, BigInteger.ZERO));
 
-        System.out.printf("Минимальное значение:  %d \n", min);
-        System.out.printf("Максимальное значение: %d \n", max);
-        System.out.printf("Среднее значение:      %d", average);
+        result[1] = Integer.parseInt(sum
+                .divide(BigInteger.valueOf(array.length))
+                .toString());
+        return result;
+    }
+
+    private static int randomizer(int previous) {
+
+        long a = 25214903917L;
+        long c = 11L;
+
+        long r = a * previous + c * new Date().getTime();
+
+        assert (r >= 0x80000000);
+        assert (r <= 0x7fffffff);
+
+        return (int) r;
+    }
+
+    private static int[][] init(int arrayLength) {
+
+        int[][] array = new int[arrayLength][arrayLength];
+
+        int previous = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+
+                int value = randomizer(previous);
+
+                array[i][j] = value;
+
+                previous = value;
+
+            }
+
+        }
+        return array;
     }
 
 }
